01ea1764df74f66fe28fbd42286701a3afcdb9a3
L4p1n45 <martynas.stake@gmail.com>
2017-09-19 10:19:05 +0300

    Migrating to Jersey 2

--- src/java/is/idega/idegaweb/member/servlet/MemberIntegrationServlet.java
+++ src/java/is/idega/idegaweb/member/servlet/MemberIntegrationServlet.java
@@ -1,7 +1,6 @@
 package is.idega.idegaweb.member.servlet;
 
 import java.io.IOException;
-import java.net.URI;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -17,11 +16,6 @@ public class MemberIntegrationServlet extends DefaultRestfulServlet {
 	private static final long serialVersionUID = -100199126638713752L;
 
 	@Override
-	public int service(URI baseUri, URI requestUri, HttpServletRequest request,	HttpServletResponse response) throws ServletException, IOException {
-		return super.service(baseUri, requestUri, request, response);
-	}
-
-	@Override
 	public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		super.service(request, response);
 	}
@@ -35,5 +29,4 @@ public class MemberIntegrationServlet extends DefaultRestfulServlet {
 	public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
 		super.doFilter(request, response, chain);
 	}
-
 }
